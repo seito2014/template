538d9db34748e6c7f54ef49115f11c6961fa4c85
堀口誠人 <horiguchi_seito@liginc.co.jp>
2015-04-22 01:11:52 +0900

    buildタスク追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
 var gulp = require('gulp');
 var requireDir = require('require-dir');
 var configPath = require('./gulp/config-path');
+var runSequence = require('run-sequence');
 
 requireDir('./gulp/tasks', { recursive: true });
 
 //watch pc
+gulp.task('build-pc', function(callback) {
+    return runSequence(['ejs-pc','style-pc','script-pc','image-pc','copy-pc'], callback);
+});
+
 gulp.task('watch-pc', function () {
     gulp.watch(configPath.pc.html.src,['ejs-pc']);
     gulp.watch(configPath.pc.style.src,['style-pc']);
     gulp.watch(configPath.pc.script.src,['script-pc']);
     gulp.watch(configPath.pc.image.src,['image-pc']);
 });
-gulp.task('default', ['server-pc','watch-pc','ejs-pc','style-pc','script-pc','image-pc','copy-pc'], function() {});
+
+gulp.task('default', ['server-pc','watch-pc'], function() {});
 
 //watch sp
+gulp.task('build-sp', function(callback) {
+    return runSequence(['ejs-sp','style-sp','script-sp','image-sp','copy-sp'], callback);
+});
+
 gulp.task('watch-sp', function () {
     gulp.watch(configPath.sp.html.src,['ejs-sp']);
     gulp.watch(configPath.sp.style.src,['style-sp']);
     gulp.watch(configPath.sp.script.src,['script-sp']);
     gulp.watch(configPath.sp.image.src,['image-sp']);
 });
-gulp.task('sp', ['server-sp','watch-sp','ejs-sp','style-sp','script-sp','image-sp','copy-sp'], function() {});
+
+gulp.task('sp', ['server-sp','watch-sp'], function() {});
