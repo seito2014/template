3aeba59115251b10af1a5c9da13e10374e97a61d
堀口誠人 <horiguchi_seito@liginc.co.jp>
2015-06-29 00:26:30 +0900

    PC OK

--- app/pc/public/js/app.js
+++ app/pc/public/js/app.js
@@ -17,7 +17,7 @@ webpackJsonp([0],[
 
 	// サイト全体で使う定数
 	module.exports = {
-	    BASE_URL:document.getElementById('base_url').value,
+	    BASE_URL:document.getElementById('base-url').value,
 	    SPEED_NORMAL:350
 	};
 
